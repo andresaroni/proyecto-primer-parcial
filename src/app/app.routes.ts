c88e1040d9db14be2afb5e9d9865f957e877d416
andresaroni <marcoaroni10@gmail.com>
2025-11-30 20:53:12 -0500

    Primer Commit del Proyecto Grupal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: "", component:Home}
+    // Configurar la ruta {path: "login", component: Login}
+    // etc asi misma logica de las rutas
+
+];
